a7e0cc5aea98d84c37785d80b7f7f2a79da4031b
ChrisCooper0 <christopher.cooper@mail.com>
2021-10-12 22:11:09 +0100

    init

---
+++ sumOfNumbers.ts
@@ -0,0 +1,14 @@
+// Write a function `total` that accepts an array of numbers as an argument. The function should return the total sum of all elements of the array.
+
+const total = (arr: number[]): number => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
+
+console.log(total([3, 2, 8])); // 13
+console.log(total([-5, 7, 4, 6])); // 12
+console.log(total([7])); // 7
+console.log(total([])); // 0
