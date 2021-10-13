c8efb45da835edb403a4699f861f05d31f42d023
ChrisCooper0 <christopher.cooper@mail.com>
2021-10-13 20:38:39 +0100

    add kata

---
+++ bleepVowels/bleepVowels.ts
@@ -0,0 +1,19 @@
+// Write a function `bleepVowels` that accepts a string as an argument. The function should return a new string where all vowels are replaced with `*`s. Vowels are the letters a, e, i, o, u.
+
+let bleepVowels = (str: string) => {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.indexOf(str[i]) > -1) {
+      newStr += "*";
+    } else {
+      newStr += str[i];
+    }
+  }
+  return newStr;
+};
+
+console.log(bleepVowels("skateboard")); // 'sk*t*b**rd'
+console.log(bleepVowels("slipper")); // 'sl*pp*r'
+console.log(bleepVowels("range")); // 'r*ng*'
+console.log(bleepVowels("brisk morning")); // 'br*sk m*rn*ng'
---
+++ positiveNumbers/positiveNumbers.ts
@@ -0,0 +1,16 @@
+// Write a function `stayPositive` that accepts an array of numbers as an argument. The function should return an array containing only the positive numbers.
+
+const positiveNumbers = (num: number[]) => {
+  const positives = [];
+
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] >= 0) {
+      positives.push(num[i]);
+    }
+  }
+  return positives;
+};
+
+console.log(positiveNumbers([10, -4, 3, 6])); // [10, 3, 6]
+console.log(positiveNumbers([-5, 11, -40, 30.3, -2])); // [11, 30.3]
+console.log(positiveNumbers([-11, -30])); // []
