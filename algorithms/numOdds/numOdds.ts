3e30f5d31817a93beae33da70b0c771733997497
ChrisCooper0 <christopher.cooper@mail.com>
2021-10-14 20:51:20 +0100

    add kata

---
+++ algorithms/numOdds/numOdds.ts
@@ -0,0 +1,15 @@
+// Write a function `numOdds` that accepts an array of numbers as an argument. The function should return a number representing the count of odd elements in the array.
+
+const numOdds = (num: number[]) => {
+  let count = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 === 1) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(numOdds([4, 7, 2, 5, 9])); // 3
+console.log(numOdds([11, 31, 58, 99, 21, 60])); // 4
+console.log(numOdds([100, 40, 4])); // 0
