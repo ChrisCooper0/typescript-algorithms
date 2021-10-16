800c89ec7ad2e02091be4742aac080d1fc98f5f2
ChrisCooper0 <christopher.cooper@mail.com>
2021-10-16 11:12:36 +0100

    update kata

--- algorithms/bleepVowels/bleepVowels.js
+++ algorithms/bleepVowels/bleepVowels.js
@@ -1,17 +1,16 @@
 "use strict";
 // Write a function `bleepVowels` that accepts a string as an argument. The function should return a new string where all vowels are replaced with `*`s. Vowels are the letters a, e, i, o, u.
 var bleepVowels = function (str) {
-    var vowels = ["a", "e", "i", "o", "u"];
-    var newStr = "";
-    for (var i = 0; i < str.length; i++) {
-        if (vowels.indexOf(str[i]) > -1) {
-            newStr += "*";
-        }
-        else {
-            newStr += str[i];
-        }
+  var vowels = ["a", "e", "i", "o", "u"];
+  var newStr = "";
+  for (var i = 0; i < str.length; i++) {
+    if (vowels.indexOf(str[i]) > -1) {
+      newStr += "*";
+    } else {
+      newStr += str[i];
     }
-    return newStr;
+  }
+  return newStr;
 };
 console.log(bleepVowels("skateboard")); // 'sk*t*b**rd'
 console.log(bleepVowels("slipper")); // 'sl*pp*r'
