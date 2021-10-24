7805dcf808ae033669e747bb8344a22981e25492
ChrisCooper0 <christopher.cooper@mail.com>
2021-10-24 19:37:52 +0100

    add kata

---
+++ algorithms/removeDupes/removeDupes.ts
@@ -0,0 +1,16 @@
+// Write a function `removeDupes` that accepts an array as an argument. The function should return a new array where each element only appears once.
+
+const removeDupes = (arr: Array<any>) => {
+  let newArr: Array<any> = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (newArr.indexOf(arr[i]) === -1) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+
+console.log(removeDupes(["x", "y", "y", "x", "z"])); // ['x', 'y', 'z']
+console.log(removeDupes([false, false, true, false])); // [false, true]
+console.log(removeDupes([42, 5, 7, 42, 7, 3, 7, 7])); // [42, 5, 7, 3]
