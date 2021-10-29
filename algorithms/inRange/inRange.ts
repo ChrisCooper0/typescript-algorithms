d24b7385a654e40e2bd30baae7460a743ac752c1
ChrisCooper0 <christopher.cooper@mail.com>
2021-10-29 21:16:36 +0100

    add kata

---
+++ algorithms/inRange/inRange.ts
@@ -0,0 +1,13 @@
+// Write a function `inRange(min, max, n)` that accepts three numbers as arguments. The function should return a boolean indicating if `n` is between `min` and `max` inclusive.
+
+let inRange = (min: number, max: number, n: number) => {
+  return n >= min && n <= max;
+};
+
+console.log(inRange(5, 13, 8)); // true
+console.log(inRange(5, 13, 29)); // false
+console.log(inRange(100, 125, 100)); // true
+console.log(inRange(100, 125, 99)); // false
+console.log(inRange(40, 45, 44)); // true
+console.log(inRange(40, 45, 45)); // true
+console.log(inRange(40, 45, 46)); // false
