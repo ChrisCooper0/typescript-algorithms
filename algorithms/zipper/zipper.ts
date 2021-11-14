e10ad194a5a4101ea43bbc36976887306040bd77
ChrisCooper0 <christopher.cooper@mail.com>
2021-11-14 11:16:45 +0000

    add examples

--- algorithms/zipper/zipper.ts
+++ algorithms/zipper/zipper.ts
@@ -8,4 +8,25 @@ const zipper = (arr1: Array<number | string>, arr2: Array<number | string>) => {
   return result;
 };
 
-console.log(zipper([1, 2, 3], ["a", "b", "c"]));
+console.log(zipper([1, 2, 3], ["a", "b", "c"])); // [ [ 1, 'a' ], [ 2, 'b' ], [ 3, 'c' ] ]
+
+let array5 = ["a", "b", "c", "d"];
+let array6 = [-1, -2, -3, -4];
+
+console.log(zipper(array5, array6));
+// [
+//   ['a', -1],
+//   ['b', -2],
+//   ['c', -3],
+//   ['d', -4],
+// ]
+
+let array7 = ["whisper", "talk", "shout"];
+let array8 = ["quiet", "normal", "loud"];
+
+console.log(zipper(array7, array8));
+// [
+//   ['whisper', 'quiet'],
+//   ['talk', 'normal'],
+//   ['shout', 'loud'],
+// ]
