a1ffdfa3e1bbf6890ad27b9ff25ec524b96532a3
ChrisCooper0 <christopher.cooper@mail.com>
2021-12-10 18:16:11 +0000

    add kata

---
+++ algorithms/adjacentElementsProduct/adjacentElementsProduct.ts
@@ -0,0 +1,16 @@
+// Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+// For inputArray = [3, 6, -2, -5, 7, 3], the output should be adjacentElementsProduct(inputArray) = 21.
+// 7 and 3 produce the largest product.
+
+const adjacentElementsProduct = (arr: number[]): number => {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const product = arr[i] * arr[i - 1];
+    if (product > 0) {
+      result = product;
+    }
+  }
+  return result;
+};
+
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3])); // 21
