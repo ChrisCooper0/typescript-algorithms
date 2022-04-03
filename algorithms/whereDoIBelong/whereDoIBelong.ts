ab71b348a856bb8854f862493bb52a80a33d7423
ChrisCooper0 <christopher.cooper@mail.com>
2022-04-03 19:31:50 +0100

    add kata

--- algorithms/whereDoIBelong/whereDoIBelong.ts
+++ algorithms/whereDoIBelong/whereDoIBelong.ts
@@ -1,12 +1,20 @@
 // Where do I Belong
-// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
-
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted.
+// The returned value should be a number.
 // For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
-
 // Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
 
-// function getIndexToIns(arr, num) {
-//     return num;
-//   }
+function getIndexToIns(arr: number[], num: number): number {
+  let sortedArr = arr.sort((a: number, b: number) => a - b);
+  console.log(sortedArr);
+  let index: number = 0;
+  for (let i = 0; i < sortedArr.length; i++) {
+    if (sortedArr[i - 1] < num && sortedArr[i] > num) {
+      index = i;
+    }
+  }
+  return index;
+}
 
-//   getIndexToIns([40, 60], 50);
+console.log(getIndexToIns([1, 2, 3, 4], 1.5)); // 1
+console.log(getIndexToIns([20, 3, 5], 19)); // 2
