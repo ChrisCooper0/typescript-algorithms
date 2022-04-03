402fcd0e01793030a2710b9e92faba53e5f40f9d
ChrisCooper0 <christopher.cooper@mail.com>
2022-04-03 19:20:23 +0100

    add kata

--- algorithms/falsyBouncer/falsyBouncer.ts
+++ algorithms/falsyBouncer/falsyBouncer.ts
@@ -3,9 +3,14 @@
 
 // Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 
-// Hint: Try converting each value to a Boolean.
-// function bouncer(arr) {
-//     return arr;
-//   }
+function bouncer(arr: any[]): any[] {
+  let falseArray: any[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Boolean(arr[i]) === false) {
+      falseArray.push(arr[i]);
+    }
+  }
+  return falseArray;
+}
 
-//   bouncer([7, "ate", "", false, 9]);
+console.log(bouncer([7, "ate", "", false, 9, undefined, NaN])); // ["", false, undefined, NaN]
