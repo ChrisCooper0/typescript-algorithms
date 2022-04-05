2d5aaf96d920363f2bdd3ec85061b309af025fc9
ChrisCooper0 <christopher.cooper@mail.com>
2022-04-05 20:37:41 +0100

    add kata

--- algorithms/sliceAndSplice/sliceAndSplice.ts
+++ algorithms/sliceAndSplice/sliceAndSplice.ts
@@ -3,8 +3,12 @@
 // Begin inserting elements at index n of the second array.
 // Return the resulting array. The input arrays should remain the same after the function runs.
 
-// function frankenSplice(arr1, arr2, n) {
-//     return arr2;
-//   }
-  
-//   frankenSplice([1, 2, 3], [4, 5, 6], 1);
+function frankenSplice(arr1: number[], arr2: number[], n: number): number[] {
+  let newArray: number[] = [];
+  newArray.push(...arr2.slice(0, n));
+  newArray.push(...arr1);
+  newArray.push(...arr2.slice(n));
+  return newArray;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1)); // [4, 1, 2, 3, 5, 6]
