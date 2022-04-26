9a6f806bdd7d0efd6df5a22d80c325493f4b9343
ChrisCooper0 <christopher.cooper@mail.com>
2022-04-26 19:59:47 +0100

    add kata

---
+++ algorithms/sumAllPrimes/sumAllPrimes.ts
@@ -0,0 +1,28 @@
+// A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+// Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+
+const sumPrimes = (num: number) => {
+  let count = 0;
+  let sum = 0;
+
+  const isPrime = (num: number) => {
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+    return num >= 2;
+  };
+
+  while (num >= count) {
+    if (isPrime(count)) {
+      sum += count;
+    }
+    count++;
+  }
+  return sum;
+};
+
+console.log(sumPrimes(10)); // 17
+console.log(sumPrimes(100)); // 1060
+console.log(sumPrimes(977)); // 73156
