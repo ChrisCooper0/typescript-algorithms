d172502b81cc052536cc1a56a06fc16f2b12957a
ChrisCooper0 <christopher.cooper@mail.com>
2023-07-23 20:01:32 +0100

    to ts

--- algorithms/counter/counter.ts
+++ algorithms/counter/counter.ts
@@ -1,14 +1,10 @@
 // Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 
-/**
- * @param {number} n
- * @return {Function} counter
- */
-var createCounter = function (n: number) {
+function createCounter(n: number): () => number {
   return function () {
     return n++;
   };
-};
+}
 
 const counter = createCounter(10);
 console.log(counter()); // 10
