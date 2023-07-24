f873d285aabce674808bd3c96c2342d3768231de
ChrisCooper0 <christopher.cooper@mail.com>
2023-07-24 18:48:32 +0100

    add lc

---
+++ algorithms/functionComposition/functionComposition.ts
@@ -0,0 +1,21 @@
+// Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+// The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+// The function composition of an empty list of functions is the identity function f(x) = x.
+
+// You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+  return function (x) {
+    for (const fn of functions.reverse()) {
+      x = fn(x);
+    }
+    return x;
+  };
+}
+
+const fn = compose([(x) => x + 1, (x) => 2 * x]);
+console.log(fn(4)); // 9
